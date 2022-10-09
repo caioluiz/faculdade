3e924ef84a657ed9dcd3452e8214c4d225f9506c
NaiKeN <caioluizsouza12@gmail.com>
2022-10-09 13:27:48 -0300

    Update: structure ++

--- C/condicionais.c
+++ C/condicionais.c
@@ -2,9 +2,12 @@
 #define mensagemNota "Insira a nota do aluno: "
 
 int main(){
-	float nota;
+	float nota = 0;
 	int s;
+	int i;
+	int j;
 	
+	// estrutura if e else
 	printf("%s\n", mensagemNota);
 	scanf("%f", &nota);
 	
@@ -15,18 +18,37 @@ int main(){
 		printf("Aluno Reprovado!\n");
 	}
 	
+	// estrutura switch-case
 	printf("Digite: 1- para Aprovado, 2- para Reprovado\n");
 	scanf("%d", &s);
 	
 	switch(s){
 		case 1:
-			printf("Parabens! Voce passou de ano com sucesso!");
+			printf("Parabens! Voce passou de ano com sucesso!\n");
 			break;
 		case 2:
-			printf("Oh Oh! Voce ficou reprovado e precisa fazer a disciplina novamente :(");
+			printf("Oh Oh! Voce ficou reprovado e precisa fazer a disciplina novamente :(\n");
 			break;
 		default:
-			printf("Algo deu errado, tente novamente mais tarde.");
+			printf("Algo deu errado, tente novamente mais tarde.\n");
 			break;
+	}
+	
+	//estrutura while
+	printf("Informe um numero inteiro menor que 10: ");
+	scanf("%i", &i);
+	
+	while(i<=10){
+		printf("%i\n", i);
+		i++;
+	}
+	
+	//estrutura for
+	for(j = 1; j <= 10; j++){
+		printf("Valor da variavel: %i\n", j);
+		
+		if(j == 5){
+			break; // aborta a missão, usar para testes. o "continue" ele pula a interação seguinte
+		}
 	}
 }
